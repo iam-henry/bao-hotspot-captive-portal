3a73f65bd643b45f08bc74b0c55084c72814e146
iam-henry <76020936+iam-henry@users.noreply.github.com>
2023-04-19 07:38:51 -0500

    bao template

---
+++ src/bao/js/session.js
@@ -0,0 +1,26 @@
+
+var session = (function() {
+    
+    const router_key = 'b4446feaf944492dab9ebe69efe41a92';
+    const router_id = '996e83d7844145cfbc7e3f1ca2832fa2';
+
+    return {
+        start: function () {
+
+            //alert('server address:' + '$(server-address)');
+            //alert('server mac: $(mac)');
+            //alert('server ip: $(ip)');
+
+            //console.log('start session');
+            //console.log('server address: $(server-address)');
+            //console.log('server mac: $(mac)');
+            //console.log('server ip: $(ip)');
+
+            document.sendin.username.value = document.login.username.value;
+            document.sendin.password.value = hexMD5('$(chap-id)' + 'd23b' + '$(chap-challenge)');
+            //document.sendin.password.value = hexMD5('$(chap-id)' + document.login.password.value + '$(chap-challenge)');
+            document.sendin.submit();
+            return;
+        }
+    };
+})();
