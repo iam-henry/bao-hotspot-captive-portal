fc3f663a5ee2d4d63fb24a090007e9bd420fbd07
iam-henry <76020936+iam-henry@users.noreply.github.com>
2023-04-20 08:16:04 -0500

    removed diagnostics

--- src/portal/js/login.js
+++ src/portal/js/login.js
@@ -6,15 +6,14 @@ var login = (function() {
             const router_key = 'b4446feaf944492dab9ebe69efe41a92';
             const router_id = '996e83d7844145cfbc7e3f1ca2832fa2';
 
-            console.log("BEGIN: dologin()");
             document.sendin.username.value = document.login.username.value;
 
             const dto = {   login: document.sendin.username.value,
                             routerId: router_id,
                             routerKey: router_key,
-                            routerIP: 'routerIp',
-                            clientMac: 'clientMac',
-                            clientIP: 'clientIp' 
+                            routerIP: routerIp,
+                            clientMac: clientMac,
+                            clientIP: clientIp 
                         };
 
             const session = JSON.stringify(dto);
@@ -26,25 +25,21 @@ var login = (function() {
             xhr.open(method, url, true);
             xhr.setRequestHeader('content-type', 'application/json;charset=UTF-8');           
             xhr.onreadystatechange = () => {
-                console.log("xhr ready state changed");
 
                 if (xhr.readyState === XMLHttpRequest.DONE) {
                     const status = xhr.status;
 
                     if (status === 0 || (status >= 200 && status < 400)) {
-                        console.log(xhr.responseText);
-                        var password = xhr.responseText.replace(/\"/g, '');
 
-                        document.sendin.password.value = password;
+                        document.sendin.password.value =  xhr.responseText.replace(/\"/g, '');
                         document.sendin.submit();
                     }
                     else {
-                        console.error("request error");
+                        console.error(`request error: ${xhr.responseText}`);
                     }
                 }
             };
 
-            console.log("sending xhr request");
             xhr.send(session);
         }
     };
